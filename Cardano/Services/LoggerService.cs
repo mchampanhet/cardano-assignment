191188b79f198de37ef2cbf48b96c2215eccfd74
Matthieu Champanhet <matthieu@globalfactories.nl>
2024-02-08 11:56:06 +0100

    add logger service

---
+++ Cardano/Services/LoggerService.cs
@@ -0,0 +1,24 @@
+﻿namespace Cardano.Services
+{
+    internal static class LoggerService
+    {
+        internal static void LogInfo(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        internal static void LogWarning(string message)
+        {
+            Console.WriteLine("Warning: " + message);
+        }
+
+        internal static void LogError(string message, Exception? ex = null)
+        {
+            Console.WriteLine("Error: " + message);
+            if (ex != null)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
